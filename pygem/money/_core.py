86f9061997028583762132ea15add92219d30816
Didou09 <didier.vezinet@cea.fr>
2018-02-25 16:16:16 +0100

    Early structure following the article

---
+++ pygem/money/_core.py
@@ -0,0 +1,21 @@
+# coding utf-8
+
+"""
+Monetary macrodynamics module of pygem
+Only mono-sector so far (only one type of good) 
+
+Definitions:
+    Y0  : amount of goods produced at each time step (a.u.)
+    K   : stock of capital used to produce the goods (money)
+    L   : Labour (hours)
+    nu  : (constant) capital-outpu ratio (money / a.u.)
+          (amount of capital necessary per output unit)
+    a   : labor-augmenting techo level (a.u. / hours)
+          (Harrod-neutral definition, cf. )
+    
+
+
+"""
+
+
+
